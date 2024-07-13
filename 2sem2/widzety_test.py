30e2f9ed28906a124444af861a879e0b8738ee46
mikoslaf <mikolaj2003wojcik@gmail.com>
2024-07-13 18:58:30 +0200

    .

--- 2sem2/widzety_test.py
+++
@@ -1,9 +0,0 @@
-from widzety import get_minimum_screen_count
-
-
-def test_all_examples():
-    with open('widget_test_data.txt', 'r') as f:
-        for ln in f.readlines():
-            x, y, answer = [int(x) for x in ln.split(' ')]
-            # print(x, y)
-            assert get_minimum_screen_count(x,y) == answer
